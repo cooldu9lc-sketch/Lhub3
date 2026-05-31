3f6c5649d380b31b8a54203a818f9027b92f9175
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-01 04:20:50 +0530

    Time: 1259 ms (13.65%), Space: 26.3 MB (17.01%) - LeetHub

---
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canPartitionKSubsets(self, arr: List[int], k: int) -> bool:
+        n=len(arr)
+        arr.sort(reverse=True)
+        target,rem =divmod(sum(arr),k)
+        if rem or arr[0]>target:return False
+        
+        dp={0:0}
+        for mask in range(1<<n):
+            if mask not in dp:continue
+            for i,num in enumerate(arr):
+                if not (mask & 1<<i) and dp[mask]+num<=target:
+                    dp[mask|1<<i]=(dp[mask]+num)%target
+ 
+        return (1<<n)-1 in dp and dp[(1<<n)-1]==0
