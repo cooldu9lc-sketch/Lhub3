250d98d7d2f4d81fa7b490f66cde7a9dbe3b395d
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-04 16:05:30 +0530

    Time: 7 ms (67.74%), Space: 21.1 MB (51.94%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,17 +1,25 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        points = {}
+        points = defaultdict(int)
         max_number = 0
-        # Precompute how many points we gain from taking an element
         for num in nums:
-            points[num] = points.get(num, 0) + num
+            points[num] += num
             max_number = max(max_number, num)
         
-        # Base cases
-        two_back = 0
-        one_back = points.get(1, 0)
-        
-        for num in range(2, max_number + 1):
-            two_back, one_back = one_back, max(one_back, two_back + points.get(num, 0))
+        two_back = one_back = 0
+        n = len(points)
+        if max_number < n + n * log(n, 2):
+            one_back = points[1]
+            for num in range(2, max_number + 1):
+                two_back, one_back = one_back, max(one_back, two_back + points[num])
+        else:
+            elements = sorted(points.keys())
+            one_back = points[elements[0]]     
+            for i in range(1, len(elements)):
+                current_element = elements[i]
+                if current_element == elements[i - 1] + 1:
+                    two_back, one_back = one_back, max(one_back, two_back + points[current_element])
+                else:
+                    two_back, one_back = one_back, one_back + points[current_element]
 
         return one_back
