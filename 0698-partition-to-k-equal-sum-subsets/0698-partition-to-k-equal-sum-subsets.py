fc8919e44ecd6bf592a1c82e6cfd3cb6ff6a03d9
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-04 15:35:25 +0530

    Time: 1259 ms (13.85%), Space: 26 MB (18.5%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,58 +1,19 @@
 class Solution:
     def canPartitionKSubsets(self, arr: List[int], k: int) -> bool:
-        n = len(arr)
-    
-        # If the total sum is not divisible by k, we can't make subsets.
-        total_array_sum = sum(arr)
-        if total_array_sum % k != 0:
+        n=len(arr)
+        if k==n:return len(set(arr))==1
+        total=sum(arr)
+        if total%k!=0:
             return False
-        target_sum = total_array_sum // k
-
-        # Sort in decreasing order.
-        arr.sort(reverse=True)
-
-        mask = 0
-        
-        memo = {}
-        
-        def backtrack(index, count, curr_sum):
-            nonlocal mask
-            n = len(arr)
-            
-            # We made k - 1 subsets with target sum and the last subset will also have target sum.
-            if count == k - 1:
-                return True
-            
-            # No need to proceed further.
-            if curr_sum > target_sum:
-                return False
-            
-            # If we have already computed the current combination.
-            if mask in memo:
-                return memo[mask]
-            
-            # When curr sum reaches target then one subset is made.
-            # Increment count and reset current sum.
-            if curr_sum == target_sum:
-                memo[mask] = backtrack(0, count + 1, 0)
-                return memo[mask]
-
-            # Try not picked elements to make some combinations.
-            for j in range(index, n):
-                if ((mask >> j) & 1) == 0:
-                    # Include this element in current subset.
-                    mask = (mask | (1 << j))
-
-                    # If using current jth element in this subset leads to make all valid subsets.
-                    if backtrack(j + 1, count, curr_sum + arr[j]):
-                        return True
-
-                    # Backtrack step.
-                    mask = (mask ^ (1 << j))
-
-            # We were not able to make a valid combination after picking 
-            # each element from the array, hence we can't make k subsets.
-            memo[mask] = False
-            return memo[mask] 
-
-        return backtrack(0, 0, 0)
+        target=total//k
+        if max(arr)>target:
+            return False
+        dp={0:0}
+        for mask in range(1<<n):
+            if mask not in dp:continue
+            for i,num in enumerate(arr):
+                if not (mask & 1<<i) and dp[mask]+num<=target:
+                    dp[mask|1<<i]=(dp[mask]+num)%target
+        if (1<<n)-1 in dp and dp[(1<<n)-1]==0:
+            return True
+        return False
