c39967c00916bec920b3619687d44dd3b1977017
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-04 15:10:00 +0530

    Time: 170 ms (49.35%), Space: 21 MB (7.92%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -1,41 +1,34 @@
-class Solution(object):
-    def largestDivisibleSubset(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+
+        def EDS(i):
+            """recursion with memoization"""
+            if i in memo:
+                return memo[i]
+
+            tail = nums[i]
+            maxSubset = []
+            # The value of EDS(i) depends on it previous elements
+            for p in range(0, i):
+                if tail % nums[p] == 0:
+                    subset = EDS(p)
+                    if len(maxSubset) < len(subset):
+                        maxSubset = subset
+
+            # extend the found max subset with the current tail.
+            maxSubset = maxSubset.copy()
+            maxSubset.append(tail)
+
+            # memorize the intermediate solutions for reuse.
+            memo[i] = maxSubset
+            return maxSubset
+
+        # test case with empty set
         if len(nums) == 0:
             return []
 
-        # important step !
         nums.sort()
+        memo = {}
 
-        # The container that keep the size of the largest divisible subset that ends with X_i
-        # dp[i] corresponds to len(EDS(X_i))
-        dp = [0] * (len(nums))
-
-        """ Build the dynamic programming matrix/vector """
-        for i, num in enumerate(nums):
-            maxSubsetSize = 0
-            for k in range(0, i):
-                if nums[i] % nums[k] == 0:
-                    maxSubsetSize = max(maxSubsetSize, dp[k])
-
-            maxSubsetSize += 1
-            dp[i] = maxSubsetSize
-
-        """ Find both the size of largest divisible set and its index """
-        maxSize, maxSizeIndex = max([(v, i) for i, v in enumerate(dp)])
-        ret = []
-
-        """ Reconstruct the largest divisible subset """
-        # currSize: the size of the current subset
-        # currTail: the last element in the current subset
-        currSize, currTail = maxSize, nums[maxSizeIndex]
-        for i in range(maxSizeIndex, -1, -1):
-            if currSize == dp[i] and currTail % nums[i] == 0:
-                ret.append(nums[i])
-                currSize -= 1
-                currTail = nums[i]
-
-        return ret[::-1]
+        # Find the largest divisible subset
+        return max([EDS(i) for i in range(len(nums))], key=len)
