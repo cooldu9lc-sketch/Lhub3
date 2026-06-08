8e0e9050adc838aa010898d95281dd2bf28ec92d
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-08 14:11:06 +0530

    Time: 0 ms (100%), Space: 19.2 MB (63.25%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -9,10 +9,15 @@ class Solution:
         dummy.next = head
         first = dummy
         second = dummy
-        # Advances first pointer so that the gap between first and second is n nodes apart
-        for i in range(n + 1):
-            first = first.next
+        # Advances first pointer from Dummy node  so that the gap between first and second is n nodes apart
+        for i in range(n + 1): # Loop runs n+1 ties from Auxilary
+            first = first.next # First is at node n+1 here
         # Move first to the end, maintaining the gap
+        # In the while loop , first positions are n+2,n+3,n+4
+        # while second positions are 1,2,3
+        ## so first and second positions are always at an n-1 difference
+        ###AFter the end of while loop
+        #### second is at L+1-n-1= L-nth Node
         while first is not None:
             first = first.next
             second = second.next
