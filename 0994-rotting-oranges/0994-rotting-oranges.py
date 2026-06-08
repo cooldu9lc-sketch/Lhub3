5e66109032ff7374390e4b2efc8e694e9481e33d
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-08 13:19:57 +0530

    Time: 7 ms (17.02%), Space: 19.2 MB (95.97%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,24 +1,33 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        
-        m,n=len(grid),len(grid[0])
-        
-        q =collections.deque([])
-        res=fresh=0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j]==2:
-                    q.append((i,j,0))
-                elif grid[i][j]==1:
-                    fresh+=1
-       
-        while fresh and q:
-            x,y,c=q.popleft()
-            for dx,dy in [(1,0),(-1,0),(0,1),(0,-1)]:
-                nx,ny=x+dx,y+dy
-                if 0<=nx<m and 0<=ny<n and grid[nx][ny]==1:
-                    fresh-=1
-                    grid[nx][ny]=2
-                    q.append((nx,ny,c+1))
-                    res=c+1
-        return res if not fresh else -1
+        ROWS, COLS = len(grid), len(grid[0])
+
+        # run the rotting process, by marking the rotten oranges with the timestamp
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+        def runRottingProcess(timestamp):
+            # flag to indicate if the rotting process should be continued
+            to_be_continued = False
+            for row in range(ROWS):
+                for col in range(COLS):
+                    if grid[row][col] == timestamp:
+                        # current contaminated cell
+                        for d in directions:
+                            n_row, n_col = row + d[0], col + d[1]
+                            if ROWS > n_row >= 0 and COLS > n_col >= 0:
+                                if grid[n_row][n_col] == 1:
+                                    # this fresh orange would be contaminated next
+                                    grid[n_row][n_col] = timestamp + 1
+                                    to_be_continued = True
+            return to_be_continued
+
+        timestamp = 2
+        while runRottingProcess(timestamp):
+            timestamp += 1
+        # end of process, to check if there are still fresh oranges left
+        for row in grid:
+            for cell in row:
+                if cell == 1:  # still got a fresh orange left
+                    return -1
+        # return elapsed minutes if no fresh orange left
+        return timestamp - 2
