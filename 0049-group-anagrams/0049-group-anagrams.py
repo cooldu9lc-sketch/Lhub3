ee6a970ead9adf59eb99aa75ba203a3cfe430243
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-08 19:08:31 +0530

    Time: 15 ms (44.22%), Space: 22.1 MB (66.92%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,38 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans: DefaultDict[tuple, List[str]] = collections.defaultdict(list)
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord("a")] += 1
-            ans[tuple(count)].append(s)
-        return list(ans.values())
+        primes = {'a': 2, 
+                  'b': 3, 
+                  'c': 5, 
+                  'd': 7, 
+                  'e': 11, 
+                  'f': 13,
+                  'g': 17,
+                  'h': 19,
+                  'i': 23,
+                  'j': 29,
+                  'k': 31,
+                  'l': 37,
+                  'm': 41,
+                  'n': 43,
+                  'o': 47,
+                  'p': 53,
+                  'q': 59,
+                  'r': 61,
+                  's': 67, 
+                  't': 71,
+                  'u': 73,
+                  'v': 79,
+                  'w': 83,
+                  'x': 89,
+                  'y': 97,
+                  'z': 101
+                 }
+        
+        
+        mapp = defaultdict(list)
+        
+        for string in strs:
+            
+            mapp[reduce(operator.mul,[primes[c] for c in string],1)].append(string)
+            
+        return list(mapp.values())
