3807076e3e6b769e14066eafdabf501aec4ea530
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-08 16:33:13 +0530

    Time: 455 ms (93.92%), Space: 20.7 MB (93.43%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        
+        trie=lambda:defaultdict(trie)
+        root=head=trie()
+        for word in words:
+            reduce(dict.__getitem__,word,root)["#"]=word
+            """node=root
+            for char in word:
+                node=node[char]
+            node["#"]=word"""
+        res=[]
+        
+        def dfs(i,j,parent):
+            #print(i,j)
+            char = board[i][j]
+            curr= parent[char]
+            board[i][j]="$"
+            wordm = curr.pop("#",None)
+            if wordm:
+                res.append(wordm)
+            
+            for dx,dy in [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]:
+                if dx<0 or dx>=m or dy<0 or dy>=n:
+                    continue
+                if board[dx][dy] in curr:
+                    dfs(dx,dy,curr)
+            
+            board[i][j]=char
+            if len(curr)==0:
+                del parent[char]
+        
+        m,n=len(board),len(board[0])
+        for r,c in product(range(m),range(n)):
+            if board[r][c] in root:
+                dfs(r,c,root)
+
+        return res
