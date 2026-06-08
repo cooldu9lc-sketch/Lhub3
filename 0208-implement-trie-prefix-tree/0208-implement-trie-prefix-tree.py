c047fe097339f66791a7964f94d582fad4547a35
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-08 15:14:18 +0530

    Time: 44 ms (36.11%), Space: 36.6 MB (14.34%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,14 +1,14 @@
 class Trie:
 
     def __init__(self):
-        self.trie=lambda:defaultdict(self.trie)
+        self.trie=lambda:defaultdict(self.trie,{"#":0})
         self.root=self.trie()
 
     def insert(self, word: str) -> None:
         curr=self.root
         for w in word:
             curr=curr[w]
-        curr["#"]=True
+        curr["#"]+=1
 
     def search(self, word: str) -> bool:
         curr=self.root
@@ -16,7 +16,7 @@ class Trie:
             if w not in curr:
                 return False
             curr=curr[w]
-        return "#" in curr
+        return curr["#"]>=1
 
     def startsWith(self, prefix: str) -> bool:
         curr=self.root
