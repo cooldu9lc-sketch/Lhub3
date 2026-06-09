2582d740dd7222ca0ceda613711d7d5adf522888
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-09 15:55:08 +0530

    Time: 372 ms (20.23%), Space: 19.5 MB (7.98%) - LeetHub

---
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findKthNumber(self, m: int, n: int, k: int) -> int:
+        def ispossible(num):
+            count = 0 
+            for val in range(1,m+1):
+                add = min(num//val,n)
+                if add ==0: #early return
+                    break
+                count+=add
+            return count>=k
+      
+        ## EACH element in arr i.e arr[i] represents a single flower
+        left,right=1,m*n
+        while left<right:
+            mid = (left+right)//2
+            if ispossible(mid):
+                right=mid
+            else:
+                left=mid+1
+        return left
