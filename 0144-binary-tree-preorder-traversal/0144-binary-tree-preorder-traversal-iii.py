320378491ab5174635d73898996655827663b01f
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-10 14:53:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (28.31%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal-iii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        node, output = root, []
+        while node:
+            if not node.left:
+                output.append(node.val)
+                node = node.right
+            else:
+                predecessor = node.left
+
+                while predecessor.right and predecessor.right is not node:
+                    predecessor = predecessor.right
+
+                if not predecessor.right:
+                    output.append(node.val)
+                    predecessor.right = node
+                    node = node.left
+                else:
+                    predecessor.right = None
+                    node = node.right
+
+        return output
