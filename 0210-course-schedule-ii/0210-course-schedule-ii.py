f9e78628c2f6bdfd6d9c3d163d62752b0ba5b2a8
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-10 21:45:32 +0530

    Time: 6 ms (32.1%), Space: 22.4 MB (7.36%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,18 +1,29 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
         G=defaultdict(set)
-        ind=defaultdict(int)
+        visited = {} ## 1:current path 2:PRevious branch
+        res=[]
         for u,v in prerequisites:
             G[v].add(u)
-            ind[u]+=1
-        q=[course for course in range(numCourses) if ind[course]==0]
-        q=deque(q)
-        res=[]
-        while q:
-            node=q.popleft()
-            res.append(node)
+      
+        def dfs(node):
+            if node not in visited:
+                visited[node]=1
+            elif visited[node]==1:
+                return False
+            elif visited[node]==2:
+                return True
             for neigh in G[node]:
-                ind[neigh]-=1
-                if ind[neigh]==0:
-                    q.append(neigh)
-        return res if len(res)==numCourses else []
+                if not dfs(neigh):
+                    return False
+            visited[node]=2
+            res.append(node)
+            return True
+            
+        return res[::-1] if all([dfs(i) for i in range(numCourses)]) and len(res)==numCourses else []
+
+
+
+
+     
