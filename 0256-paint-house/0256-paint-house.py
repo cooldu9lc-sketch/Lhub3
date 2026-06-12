beb018966528498305368266565034a0b7efae44
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-12 20:40:30 +0530

    Time: 0 ms (100%), Space: 19.4 MB (29.25%) - LeetHub

--- 0256-paint-house/0256-paint-house.py
+++ 0256-paint-house/0256-paint-house.py
@@ -1,6 +1,6 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-        a=b=c=0
+        a=b=c=0 ## Here a,b,c are 3 different states or choices for dp[i]
         for x,y,z in costs:
             a,b,c=x+min(b,c),y+min(a,c),z+min(a,b)
         return min(a,b,c)
