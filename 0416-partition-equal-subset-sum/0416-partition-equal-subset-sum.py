c052b69dd66708b9fbd5d64a8bfd23d1881cb7b4
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-12 14:46:08 +0530

    Time: 638 ms (67.97%), Space: 19.3 MB (85.76%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -7,16 +7,12 @@ class Solution:
         if total_sum % 2 != 0:
             return False
         subset_sum = total_sum // 2
-        n = len(nums)
 
-        # construct a dp table of size (n+1) x (subset_sum + 1)
-        dp = [[False] * (subset_sum + 1) for _ in range(n + 1)]
-        dp[0][0] = True
-        for i in range(1, n + 1):
-            curr = nums[i - 1]
-            for j in range(subset_sum + 1):
-                if j < curr:
-                    dp[i][j] = dp[i - 1][j]
-                else:
-                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - curr]
-        return dp[n][subset_sum]
+        # construct a dp table of size (subset_sum + 1)
+        dp = [False] * (subset_sum + 1)
+        dp[0] = True
+        for curr in nums:
+            for j in range(subset_sum, curr - 1, -1):
+                dp[j] = dp[j] or dp[j - curr]
+
+        return dp[subset_sum]
