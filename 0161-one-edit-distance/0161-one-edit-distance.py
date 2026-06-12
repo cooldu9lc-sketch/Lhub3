d326913b9d4b08237f85609db941b434f812bf98
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-12 17:35:46 +0530

    Time: 1 ms (38.83%), Space: 19.5 MB (11.36%) - LeetHub

---
+++ 0161-one-edit-distance/0161-one-edit-distance.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isOneEditDistance(self, s: str, t: str) -> bool:
+        if len(s)>len(t):
+            s,t=t,s
+        if len(t)-len(s)>1:
+            return False
+        edited=False
+        i=j=0
+        while i<len(s):
+            if s[i]==t[j]:
+                i+=1
+                j+=1
+                continue
+            if edited:
+                return False
+            edited=True
+            if len(s)==len(t):
+                i+=1
+                j+=1
+            else:
+                j+=1
+        return (j==len(t)-1 and not edited) or (j==len(t) and edited) or (len(s)==len(t) and edited)
