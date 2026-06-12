e66ee10adc0752df1aa0443451ea3a19fdc05326
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-12 21:13:56 +0530

    Time: 0 ms (100%), Space: 19.1 MB (89.39%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.py
+++ 0276-paint-fence/0276-paint-fence.py
@@ -2,8 +2,8 @@ class Solution:
     def numWays(self, n: int, k: int) -> int:
         if n<=2:
             return k**n
-        two_back,one_back=k,k*k
+        a,b=k,k*k
         for i in range(3,n+1):
-            curr=(k-1)*(two_back+one_back)
-            one_back,two_back=curr,one_back
-        return one_back
+            curr=(k-1)*(a+b)
+            a,b=b,curr
+        return b
