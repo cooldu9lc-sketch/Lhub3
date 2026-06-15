2f8e03d33f10ae0ed9a7960aac7be107e4da2159
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-16 00:21:26 +0530

    Time: 8 ms (17.49%), Space: 20 MB (27.72%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -4,13 +4,16 @@ class Solution:
         for u,v in tickets:
             G[u].append(v)
         for u,v in tickets:
-            G[v].sort(reverse=True)
+            G[u].sort(reverse=True)
         
       
         def dfs(node):
-            res.append(node)
+            #res.append(node)
             while len(G[node]):
-                dfs(G[node].pop())
+                neigh = G[node].pop()
+                dfs(neigh)
+            res.append(node)
+
         res=[]
         dfs("JFK")
-        return res
+        return res[::-1]
