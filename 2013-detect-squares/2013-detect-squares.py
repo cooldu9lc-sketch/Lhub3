a1923a82577a8c6f3550b366998be56b3e9eca05
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-15 23:11:48 +0530

    Time: 191 ms (23.69%), Space: 20.9 MB (91.38%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.py
+++ 2013-detect-squares/2013-detect-squares.py
@@ -1,15 +1,27 @@
 class DetectSquares:
     def __init__(self):
-        self.cntPoints = Counter()
+        self.xPoints = defaultdict(list)
+        self.cnt = defaultdict(int)
 
     def add(self, point: List[int]) -> None:
-        self.cntPoints[tuple(point)] += 1
+        x, y = point
+        self.xPoints[x].append(y)
+        self.cnt[(x, y)] += 1
 
     def count(self, point: List[int]) -> int:
-        ans = 0
         x1, y1 = point
-        for (x3, y3), cnt in self.cntPoints.items():
-            if abs(x1 - x3) == 0 or abs(x1 - x3) != abs(y1 - y3):
-                continue  # Skip empty square or invalid square point!
-            ans += cnt * self.cntPoints[(x1, y3)] * self.cntPoints[(x3, y1)]
+        ans = 0
+        for y2 in self.xPoints[x1]:
+            if y2 == y1: continue  # Skip empty square
+            sideLen = abs(y2 - y1)
+
+            # Case: p3, p4 points are in the left side
+            x3, y3 = x1 - sideLen, y2
+            x4, y4 = x1 - sideLen, y1
+            ans += self.cnt[(x3, y3)] * self.cnt[(x4, y4)]
+
+            # Case 2: p3, p4 points are in the left side
+            x3, y3 = x1 + sideLen, y2
+            x4, y4 = x1 + sideLen, y1
+            ans += self.cnt[(x3, y3)] * self.cnt[(x4, y4)]
         return ans
