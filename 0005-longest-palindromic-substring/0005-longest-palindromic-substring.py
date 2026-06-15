8b19813f5f94f76c32954fa9b48c1f8e940c480a
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-15 18:38:14 +0530

    Time: 1258 ms (28.64%), Space: 27.4 MB (5.58%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,17 +1,25 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def expandFromCenter(s,l,r):
-            while l >=0 and r < len(s) and s[l] ==s[r]:
-                l -= 1
-                r += 1
-
-            if self.maxlen < r-l-1:
-                self.maxlen = r-l-1
-                self.start = l + 1
-        self.maxlen = 0
-        self.start = 0
+        maxlen =1
+        startIdx=0
+        dp = [[0]*len(s) for _ in range(len(s))]
+        #filling out the diagonal by 1
         for i in range(len(s)):
-            expandFromCenter(s,i,i)
-            expandFromCenter(s,i,i+1)
-        return s[self.start:self.start+self.maxlen]
-    
+            dp[i][i] = True
+            
+			
+        # filling the dp table
+        for i in range(len(s)-1,-1,-1):
+				# j starts from the i location : to only work on the upper side of the diagonal 
+            for j in range(i+1,len(s)):  
+                if s[i] == s[j]:  #if the chars mathces
+                    # if len slicied sub_string is just one letter if the characters are equal, we can say they are palindomr dp[i][j] =True 
+                    #if the slicied sub_string is longer than 1, then we should check if the inner string is also palindrom (check dp[i+1][j-1] is True)
+                    if j-i ==1 or dp[i+1][j-1] is True:
+                        dp[i][j] = True
+                        # we also need to keep track of the maximum palindrom sequence 
+                        if maxlen < j-i+1:
+                            maxlen=j-i+1
+                            startIdx=i
+                
+        return s[startIdx:startIdx+maxlen]
