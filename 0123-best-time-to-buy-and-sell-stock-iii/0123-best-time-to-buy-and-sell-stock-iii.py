c312ea20f03e43cc69ddc40447ce652f20166c31
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-16 16:58:59 +0530

    Time: 99 ms (69.8%), Space: 30.8 MB (92.25%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -4,15 +4,18 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        if not prices:
-            return 0
-        k = 2
-        states = [0] + [float('-inf')] * (2*k) 
-       
- 
-        for p in prices:
-            for j in range(k):
-                states[2*j+1] = max(states[2*j+1], states[2*j]-p)
-                states[2*j+2] =   max(states[2*j+2], states[2*j+1]+p)
         
-        return max(0, states[-1])
+        buy1 = float('-inf')
+        buy2 = float('-inf')
+
+        sell1 = 0
+        sell2 = 0
+
+        for p in prices:
+            buy1 = max(buy1, -p)
+            sell1 = max(sell1, buy1 + p)
+
+            buy2 = max(buy2, sell1 - p)
+            sell2 = max(sell2, buy2 + p)
+
+        return sell2
