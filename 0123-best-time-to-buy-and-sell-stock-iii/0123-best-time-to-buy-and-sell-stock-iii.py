8765587b071db3b0d849b53ff9702dbccb4f18f3
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-16 16:57:05 +0530

    Time: 280 ms (31.05%), Space: 31.1 MB (43.47%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        k = 2
+        states = [0] + [float('-inf')] * (2*k) 
+       
+ 
+        for p in prices:
+            for j in range(k):
+                states[2*j+1] = max(states[2*j+1], states[2*j]-p)
+                states[2*j+2] =   max(states[2*j+2], states[2*j+1]+p)
+        
+        return max(0, states[-1])
