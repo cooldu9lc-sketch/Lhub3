ca77b2d95ffff84237e9285fe0f46120fa9e6219
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-16 16:05:03 +0530

    Time: 65 ms (90.5%), Space: 20.8 MB (35.45%) - LeetHub

--- 0759-employee-free-time/0759-employee-free-time.py
+++ 0759-employee-free-time/0759-employee-free-time.py
@@ -8,10 +8,27 @@ class Interval:
 
 class Solution:
     def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
-        ints = sorted([i for s in schedule for i in s], key=lambda x: x.start)
-        res, end = [], ints[0].end
-        for i in ints[1:]:
-            if i.start > end:
-                res.append(Interval(end, i.start))
-            end = max(end, i.end)
+         # idea: similar like merge interval + merge k lists
+        #       put the first interval, employee idx and interval idx in a heap
+        #       if next interval is not overlapping, calc the gap and save in result
+        #       take the next interval and repeat merging.
+        
+        min_heap, res = [], []
+        
+        for i in range(len(schedule)):
+            # the last elem in tuple is next idx of interval for the employee
+            heapq.heappush(min_heap, (schedule[i][0].start, schedule[i][0].end, i, 1))
+        
+        end = min_heap[0][1]
+        
+        while min_heap:
+            new_start, new_end, i, j = heapq.heappop(min_heap)
+            if new_start <= end:
+                end = max(end, new_end)
+            else:
+                res.append(Interval(end, new_start))
+                end = new_end
+            if j < len(schedule[i]):
+                heapq.heappush(min_heap, (schedule[i][j].start, schedule[i][j].end, i, j+1))
+        
         return res
