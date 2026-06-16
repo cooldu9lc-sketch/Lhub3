2e75fab3762d918932f59300909aa3365be199cf
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-16 19:20:27 +0530

    Time: 106 ms (24.8%), Space: 23.1 MB (20.45%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -1,6 +1,6 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        @cache
+        """@cache
         def recur(i,j):
             res= float("inf")
             if i==len(word1) and j==len(word2):
@@ -13,5 +13,19 @@ class Solution:
                 res= recur(i+1,j+1)
             res= min(res,recur(i+1,j)+1,recur(i,j+1)+1)
             return res  
-        return recur(0,0)  
-
+        return recur(0,0)  """
+        m,n=len(word1),len(word2)
+        if m==0 or n==0:
+            return max(m,n)
+        dp=[[float("inf")]*(n+1) for _ in range(m+1)]
+        dp[0][0]=0
+        for j in range(1,n+1):
+            dp[0][j]=j
+        for i in range(1,m+1):
+            dp[i][0]=i
+            for j in range(1,n+1):
+                if word1[i-1]==word2[j-1]:
+                    dp[i][j]=min(dp[i][j],dp[i-1][j-1])
+                else:
+                    dp[i][j]=min(dp[i][j],dp[i-1][j]+1,dp[i][j-1]+1)
+        return dp[-1][-1]
