14d2db4be418504a602d1e14eb61ed7434d5b233
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-16 15:38:10 +0530

    Time: 69 ms (75.57%), Space: 20.7 MB (84.91%) - LeetHub

---
+++ 0759-employee-free-time/0759-employee-free-time.py
@@ -0,0 +1,36 @@
+"""
+# Definition for an Interval.
+class Interval:
+    def __init__(self, start: int = None, end: int = None):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+        OPEN, CLOSE = 1, 2
+        events = []
+        
+        for lst in schedule:
+            for interv in lst:
+                events.append((interv.start, OPEN))
+                events.append((interv.end, CLOSE))
+        
+        balance = 0
+        events.sort()
+        ans = []
+        prev = None
+        
+        for time, eventType in events:
+            if balance == 0 and prev is not None:
+                ans.append(Interval(prev, time))
+            
+            if eventType == OPEN:
+                balance += 1
+            else:
+                balance -= 1
+            
+            if balance == 0:
+                prev = time
+        
+        return ans
