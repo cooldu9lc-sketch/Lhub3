a42d68555780c12b02a8d36ea8771edefe99b170
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-16 15:42:02 +0530

    Time: 72 ms (62.16%), Space: 20.7 MB (84.91%) - LeetHub

--- 0759-employee-free-time/0759-employee-free-time.py
+++ 0759-employee-free-time/0759-employee-free-time.py
@@ -8,29 +8,10 @@ class Interval:
 
 class Solution:
     def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
-        OPEN, CLOSE = 1, 2
-        events = []
-        
-        for lst in schedule:
-            for interv in lst:
-                events.append((interv.start, OPEN))
-                events.append((interv.end, CLOSE))
-        
-        balance = 0
-        events.sort()
-        ans = []
-        prev = None
-        
-        for time, eventType in events:
-            if balance == 0 and prev is not None:
-                ans.append(Interval(prev, time))
-            
-            if eventType == OPEN:
-                balance += 1
-            else:
-                balance -= 1
-            
-            if balance == 0:
-                prev = time
-        
-        return ans
+        ints = sorted([i for s in schedule for i in s], key=lambda x: x.start)
+        res, end = [], ints[0].end
+        for i in ints[1:]:
+            if i.start > end:
+                res.append(Interval(end, i.start))
+            end = max(end, i.end)
+        return res
