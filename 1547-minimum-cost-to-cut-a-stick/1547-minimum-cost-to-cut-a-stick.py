10d3c020e0f0c1552c292c5fdbf60052fb0d8d5e
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-19 15:43:56 +0530

    Time: 337 ms (74.91%), Space: 19.5 MB (83.95%) - LeetHub

---
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minCost(self, n: int, cuts: List[int]) -> int:
+        cuts = [0] + sorted(cuts) + [n]
+        m = len(cuts)
+
+        dp = [[0] * m for _ in range(m)]
+
+        # gap = r - l
+        # gap = 1 means no internal cut => cost 0
+        for gap in range(2, m):
+            for l in range(0, m - gap):
+                r = l + gap
+                dp[l][r] = float('inf')
+
+                for k in range(l + 1, r):
+                    cost = dp[l][k] + dp[k][r] + (cuts[r] - cuts[l])
+                    dp[l][r] = min(dp[l][r], cost)
+
+        return dp[0][m - 1]
