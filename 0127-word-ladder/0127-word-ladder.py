f8f3ce66d2bfde5e9372c57feb38979d42bbdb96
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-19 23:11:56 +0530

    Time: 31 ms (98.56%), Space: 23.3 MB (15.7%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,5 +1,6 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
         if endWord not in wordList: return 0
         n=len(beginWord)
 
@@ -9,15 +10,35 @@ class Solution:
             for i in range(n):
                 word_map[word[:i]+"*"+word[i+1:]].append(word)
 
-        seen=set()
-        q=deque([(beginWord,1)])
-        while q:
-            word,level= q.popleft()
-            if word==endWord:return level
-            elif word in seen:continue
-            seen.add(word)
+        visited1= defaultdict(int)
+        visited2 = defaultdict(int)
+        q1=deque([(beginWord,0)])
+        q2=deque([(endWord,0)])
+        while q1 and q2:
+            word,level= q1.popleft()
+            if word in visited2:
+                return level + visited2[word]+1
+          
+            visited1[word]=level
+            
             for i in range(n):
                 for nextword in word_map[word[:i]+"*"+word[i+1:]]:
-                    if nextword not in seen:
-                        q.append((nextword,level+1))
+                    if nextword not in visited1:
+                        visited1[nextword] =level+1
+                        q1.append((nextword,level+1))
+
+            word,level= q2.popleft()
+            if word in visited1:
+                return level + visited1[word]+1
+            
+            visited2[word]=level
+            
+            for i in range(n):
+                for nextword in word_map[word[:i]+"*"+word[i+1:]]:
+                    if nextword not in visited2:
+                        visited2[nextword]=level+1
+                        q2.append((nextword,level+1))
+
+
+            
         return 0
