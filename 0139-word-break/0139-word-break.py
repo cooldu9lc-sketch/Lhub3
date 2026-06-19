2ead5e71fb8800a70ac87ae97c81dff0a2bdc376
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-20 00:30:45 +0530

    Time: 7 ms (23.63%), Space: 19.3 MB (51.21%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,25 +1,16 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        trie = lambda : defaultdict(trie)
-        root=trie()
-        
-        for word in wordDict:
-            reduce(dict.__getitem__,word,root)["#"] = True 
-        
-        
-        dp = [False]*(len(s))
+        n=len(s)
+        wordset = set(wordDict)
 
-      
+        dp = [False]*(n+1)
 
-        for i in range(len(s)):
-            if i == 0 or dp[i - 1]:
-                curr = root
-                for j in range(i, len(s)):
-                    if s[j] in curr:
-                        curr= curr[s[j]]
-                        if "#" in curr:dp[j]=True
-                    else:
-                        break
+        dp[0]=True
 
+        for end in range(1,n+1):
+            for start in range(end):
+                if  dp[start] and s[start:end] in wordset:
+                    dp[end]=True
+                    break
         return dp[-1]
