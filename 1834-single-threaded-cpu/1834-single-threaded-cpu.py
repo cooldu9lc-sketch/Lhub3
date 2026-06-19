96037801b7d13a37d1cc936b18db82686df65629
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-19 16:52:20 +0530

    Time: 291 ms (81.89%), Space: 65.3 MB (49.82%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -1,24 +1,19 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
         
-        ##sort tasks by their start time
-        tasks_sorted=sorted(enumerate(tasks),key=lambda x:x[1])
-        heap,res=[],[]
-        n=len(tasks)
-        i=time=0
-        while heap or i<n:
-            
-            while i<n and tasks_sorted[i][1][0]<=time:
-                idx,(eqt,pt)=tasks_sorted[i]
-                heapq.heappush(heap,(pt,idx))
-                i+=1
-            if not heap:###no tasks are available. Spo process the next available task
-                idx,(eqt,pt)=tasks_sorted[i]
-                res.append(idx)
-                time=eqt+pt
-                i+=1
-            else:
-                pt,idx=heapq.heappop(heap)
-                res.append(idx)
-                time+=pt
+        res = []
+        tasks = sorted([(t[0], t[1], i) for i, t in enumerate(tasks)])
+        i = 0
+        h = []
+        time = tasks[0][0]
+        while len(res) < len(tasks):
+            while (i < len(tasks)) and (tasks[i][0] <= time):
+                heapq.heappush(h, (tasks[i][1], tasks[i][2])) # (processing_time, original_index)
+                i += 1
+            if h:
+                t_diff, original_index = heapq.heappop(h)
+                time += t_diff
+                res.append(original_index)
+            elif i < len(tasks): ##heap is empty, next task starts at tasks[i+1]
+                time = tasks[i][0]
         return res
