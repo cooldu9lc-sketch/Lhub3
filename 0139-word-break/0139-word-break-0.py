b8a0d71f21858d6e2145556154c0c2ba3d35ae3e
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-20 00:43:29 +0530

    Update 0139-word-break-0.py

--- 0139-word-break/0139-word-break-0.py
+++ 0139-word-break/0139-word-break-0.py
@@ -1,16 +1,15 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
-        n=len(s)
-        wordset = set(wordDict)
+        dp = [False] * len(s)
+        for i in range(len(s)):
+            for word in wordDict:
+                # Handle out of bounds case
+                if i < len(word) - 1:
+                    continue
 
-        dp = [False]*(n+1)
+                if i == len(word) - 1 or dp[i - len(word)]:
+                    if s[i - len(word) + 1 : i + 1] == word:
+                        dp[i] = True
+                        break
 
-        dp[0]=True
-
-        for end in range(1,n+1):
-            for start in range(end):
-                if  dp[start] and s[start:end] in wordset:
-                    dp[end]=True
-                    break
         return dp[-1]
