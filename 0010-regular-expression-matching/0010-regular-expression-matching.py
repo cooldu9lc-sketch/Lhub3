0b57e28b54d8980c2ab42a00d8c7b5b4e5f88d4b
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-19 19:06:58 +0530

    Time: 8 ms (35.49%), Space: 21 MB (5.5%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,16 +1,23 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        m,n=len(s),len(p)
-        dp=[[False]*(n+1) for _ in range(m+1)]
-        dp[0][0]=True
-        for j in range(2,n+1):
-           dp[0][j]=dp[0][j-2] and p[j-1]=="*"
-        for i in range(1,m+1):
-            for j in range(1,n+1):
-                if s[i-1]==p[j-1] or p[j-1]==".":
-                    dp[i][j]=dp[i-1][j-1]
-                elif p[j-1]=="*":
-                  dp[i][j]=dp[i][j-2] or (s[i-1]==p[j-2] or p[j-2]==".") and dp[i-1][j]
-                     
-                
-        return dp[m][n]
+        @cache
+        def dp(i,j):
+
+            if j == len(p):
+                return i == len(s)
+
+            match = (i < len(s)
+                and
+                (s[i] == p[j] or p[j] == '.')
+            )
+
+            if j+1 < len(p) and p[j+1] == '*':
+
+                return (
+                    dp(i,j+2)           # skip
+                    or
+                    (match and dp(i+1,j))
+                )
+
+            return match and dp(i+1,j+1)
+        return dp(0,0)
