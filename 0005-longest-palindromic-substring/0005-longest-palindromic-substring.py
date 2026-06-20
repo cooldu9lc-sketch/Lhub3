dbd2f13a329ceb2b8570fa3a63aaeed5efbc6e55
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-21 01:42:37 +0530

    Update 0005-longest-palindromic-substring.py

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,19 +1,17 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        # Initialize an N x N table with False
-        dp = [[False] * n for _ in range(n)]
-        maxlen=1
-        startIdx=0
-        
-        # i must go backwards to ensure inner substrings are calculated first
-        for i in range(n - 1, -1, -1):
-            for j in range(i, n):
-                # Transition function matching our template
-                if s[i] == s[j] and (j - i <= 1 or dp[i + 1][j - 1]):
-                    dp[i][j] = True
-                    if j-i+1>maxlen:
-                        maxlen=j-i+1
-                        startIdx=i
-                    
-        return s[startIdx:startIdx+maxlen]
+        def expandFromCenter(s,l,r):
+            while l >=0 and r < len(s) and s[l] ==s[r]:
+                l -= 1
+                r += 1
+
+            if self.maxlen < r-l-1:
+                self.maxlen = r-l-1
+                self.start = l + 1
+        self.maxlen = 0
+        self.start = 0
+        for i in range(len(s)):
+            expandFromCenter(s,i,i)
+            expandFromCenter(s,i,i+1)
+        return s[self.start:self.start+self.maxlen]
+    
