f3df20dd3f8090dec10bdfbd550f32b6b4115627
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-21 01:53:41 +0530

    Time: 391 ms (75.16%), Space: 19.3 MB (81.87%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -3,12 +3,13 @@ class Solution:
         
         def findcut(start,end,i):
             while start>=0 and end<len(s) and s[start]==s[end]:
-                dp[end]=min(dp[end],dp[start-1]+1)
+                if start==0: dp[end]=0 
+                else: dp[end]=min(dp[end],dp[start-1]+1)
                 start-=1
                 end+=1
         
         
-        dp=[i for i in range(len(s))]+[-1]
+        dp=[i for i in range(len(s))]
         for i in range(len(s)):
             findcut(i,i,i)
             findcut(i-1,i,i)
