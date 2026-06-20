c253243d9bf67babb479f72abd6f131676eedfb4
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-20 23:38:54 +0530

    Time: 54 ms (10.83%), Space: 19.3 MB (29.6%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,8 +1,15 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        ret, power = 0, 31
+        res=0
+        for i in range(32):
+            bit= n&1
+            res+= (bit<<(31-i))
+            n= n>>1
+        return res
+        
+        """ret, power = 0, 31
         while n:
             ret += (n & 1) << power
             n = n >> 1
             power -= 1
-        return ret
+        return ret"""
