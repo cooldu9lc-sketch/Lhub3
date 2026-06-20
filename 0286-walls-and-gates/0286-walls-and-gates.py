67857dd4ed3dbba4249d728c7c6e441178c14da8
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-20 22:37:32 +0530

    Time: 30 ms (80.88%), Space: 22.2 MB (53.28%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -10,10 +10,12 @@ class Solution:
             for j in range(n):
                 if rooms[i][j]==gate:
                     q.append((0,i,j))
+                    
+
         while q:
             dist,r,c=q.popleft()
             for nr,nc in [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]:
                 if 0<=nr<m and 0<=nc<n and rooms[nr][nc]==empty:
                     rooms[nr][nc]=dist+1
                     q.append((dist+1,nr,nc))
-    
+        
