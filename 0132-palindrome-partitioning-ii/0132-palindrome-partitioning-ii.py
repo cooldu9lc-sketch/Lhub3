4c13c622f283b5403e8a49c9dddbc32146294506
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-21 02:30:43 +0530

    Time: 646 ms (35.07%), Space: 50.6 MB (49.46%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -6,7 +6,7 @@ class Solution:
         dp = [[False] * n for _ in range(n)]
         for i in range(n - 1, -1, -1):
             for j in range(i, n):
-                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                if s[i] == s[j] and (j - i <= 1 or dp[i + 1][j - 1]):
                     dp[i][j] = True
                     
         # 2. Compute Minimum Cuts (1D DP)
