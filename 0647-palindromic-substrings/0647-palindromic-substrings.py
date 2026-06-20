8c96092779e3c340751f7d043a8d251fdb49f403
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-21 01:35:32 +0530

    Time: 183 ms (31.92%), Space: 27.3 MB (14.27%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -2,13 +2,17 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
 
 
-        def expandFromCenter(s,l,r):
-            ans=0
-            while l >=0 and r < len(s) and s[l] ==s[r]:
-                l -= 1
-                r += 1
-                ans+=1
-            return ans
-                
-        return sum(expandFromCenter(s,i,i)+expandFromCenter(s,i,i+1)  for i in range(len(s)))       
+        n = len(s)
+        # Initialize an N x N table with False
+        dp = [[False] * n for _ in range(n)]
+        count = 0
         
+        # i must go backwards to ensure inner substrings are calculated first
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                # Transition function matching our template
+                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+                    count += 1
+                    
+        return count
