9154f1b5b02c55fef7ba45b76c41322e912cced5
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-25 16:28:38 +0530

    Time: 1 ms (82.51%), Space: 19.2 MB (87.07%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -1,11 +1,5 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        shift_count = 0
-    # Keep shifting right until the prefixes match
-        while left != right:
-            left >>= 1
-            right >>= 1
-            shift_count += 1
-            
-        # Shift the common prefix back to its original position
-        return left << shift_count
+        while right>left:
+            right = right & (right-1)
+        return right
