8b3b946a1c014e1ff0d018fdb3382bc86b584a67
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-25 17:32:01 +0530

    Time: 1267 ms (88.98%), Space: 68.8 MB (55.44%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.py
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.py
@@ -4,9 +4,12 @@ class Solution:
         L=len(bin(max(nums))) - 2
         mask=ans=0
         for i in range(L-1,-1,-1):
-            mask|=1<<i
+            mask|=1<<i ###all ones
             prefixes={mask & num for num in nums}
             required=ans|1<<i
+            ## we need to find two numbers p1 and p2 in prefixes such that p1^p2=required
+            ## meaning p1^required = p2
+            
             if any(required^prefix in prefixes for prefix in prefixes):
                 ans=required
            
