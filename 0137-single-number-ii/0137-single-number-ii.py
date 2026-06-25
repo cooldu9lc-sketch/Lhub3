0d73b8becec7ef5a0ce92e604a8d796ef38759d2
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-25 16:21:33 +0530

    Time: 0 ms (100%), Space: 20.7 MB (33.65%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,25 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
-        # Loner. 
-        loner = 0
-        
-        # Iterate over all bits
-        for shift in range(32):
-            bit_sum = 0
-            
-            # For this bit, iterate over all integers
-            for num in nums:
-                
-                # Compute the bit of num, and add it to bit_sum
-                bit_sum += (num >> shift) & 1
-            
-            # Compute the bit of loner and place it
-            loner_bit = bit_sum % 3
-            loner = loner | (loner_bit << shift)
 
-        if loner & (1<<31):
-            return loner - (1<<32)
-        return loner
-        
-      
+        # Initialize seen_once and seen_twice to 0
+        seen_once = seen_twice = 0
+
+        # Iterate through nums
+        for num in nums:
+            # Update using derived equations
+            seen_once = (seen_once ^ num) & (~seen_twice)
+            seen_twice = (seen_twice ^ num) & (~seen_once)
+
+        # Return integer which appears exactly once
+        return seen_once
