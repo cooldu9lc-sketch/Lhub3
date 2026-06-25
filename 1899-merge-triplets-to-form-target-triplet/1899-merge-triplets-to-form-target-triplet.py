a894e1c58e576fac125b40f810002e8708f0579e
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-06-25 18:41:11 +0530

    Time: 34 ms (37.02%), Space: 53.9 MB (83.43%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -1,5 +1,7 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        #Consider only triplets that do not exceed the target in any dimension.
+        
         a=b=c=0
         for x,y,z in triplets:
             if x<=target[0] and y<=target[1] and z<=target[2]:
