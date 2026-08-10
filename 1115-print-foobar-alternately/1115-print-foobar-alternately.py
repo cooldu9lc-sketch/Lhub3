cf72b6036ab583db25affb69fc2dbfd9a195a24e
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-08-10 17:53:53 +0530

    Time: 58 ms (97.24%), Space: 19.8 MB (81.47%) - LeetHub

--- 1115-print-foobar-alternately/1115-print-foobar-alternately.py
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.py
@@ -1,25 +1,24 @@
 import threading
 class FooBar:
     def __init__(self, n):
-        self.n=n
-        self.foo_event= threading.Event()
-        self.bar_event = threading.Event()
-        self.foo_event.set()
+        self.n = n
+        self.foolock = threading.Lock()
+        self.barlock = threading.Lock()
+        self.barlock.acquire()
 
     def foo(self, printFoo: 'Callable[[], None]') -> None:
         
         for i in range(self.n):
-            self.foo_event.wait()
+            self.foolock.acquire()
             # printFoo() outputs "foo". Do not change or remove this line.
             printFoo()
-            self.foo_event.clear()
-            self.bar_event.set()
+            self.barlock.release()
+
 
     def bar(self, printBar: 'Callable[[], None]') -> None:
         
         for i in range(self.n):
-            self.bar_event.wait()
+            self.barlock.acquire()
             # printBar() outputs "bar". Do not change or remove this line.
             printBar()
-            self.bar_event.clear()
-            self.foo_event.set()
+            self.foolock.release()
