381696cef46689dffbb24a7bda25af3823e48692
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-09-21 00:40:11 +0530

    Time: 152 ms (96.77%), Space: 19.5 MB (55.72%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -1,34 +1,33 @@
-class Solution:
-    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-
-        def EDS(i):
-            """recursion with memoization"""
-            if i in memo:
-                return memo[i]
-
-            tail = nums[i]
-            maxSubset = []
-            # The value of EDS(i) depends on it previous elements
-            for p in range(0, i):
-                if tail % nums[p] == 0:
-                    subset = EDS(p)
-                    if len(maxSubset) < len(subset):
-                        maxSubset = subset
-
-            # extend the found max subset with the current tail.
-            maxSubset = maxSubset.copy()
-            maxSubset.append(tail)
-
-            # memorize the intermediate solutions for reuse.
-            memo[i] = maxSubset
-            return maxSubset
-
-        # test case with empty set
+class Solution(object):
+    def largestDivisibleSubset(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
         if len(nums) == 0:
             return []
 
+        # important step !
         nums.sort()
-        memo = {}
 
-        # Find the largest divisible subset
-        return max([EDS(i) for i in range(len(nums))], key=len)
+        # The container that keep the size of the largest divisible subset that ends with X_i
+        # dp[i] corresponds to len(EDS(X_i))
+        dp = [0] * (len(nums))
+        parent= [-1]*(len(nums))
+       
+
+        """ Build the dynamic programming matrix/vector """
+        for i, num in enumerate(nums):
+            for k in range(0, i):
+                if num % nums[k] == 0 and dp[k]+1>dp[i]:
+                    dp[i] = dp[k]+1
+                    parent[i]=k
+        ret=[]
+        maxSubsetSize = max(dp)
+        idx = dp.index(maxSubsetSize)
+        while parent[idx]>-1:
+            ret.append(nums[idx])
+            idx=parent[idx]
+        ret.append(nums[idx])
+        return ret[::-1]
+
