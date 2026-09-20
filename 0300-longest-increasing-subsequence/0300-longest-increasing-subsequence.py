cb3e02295a3a67bffb231ea476a603c072fe42b8
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-09-20 22:31:40 +0530

    Time: 47 ms (72.89%), Space: 19.5 MB (33.45%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,15 +1,22 @@
-class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        sub = []
-        for num in nums:
-            i = bisect_left(sub, num)
+class MaxBIT:  # One-based indexing
+    def __init__(self, size):
+        self.bit = [0] * (size + 1)
+    def get(self, idx):
+        ans = 0
+        while idx > 0:
+            ans = max(ans, self.bit[idx])
+            idx -= idx & (-idx)
+        return ans
+    def update(self, idx, val):
+        while idx < len(self.bit):
+            self.bit[idx] = max(self.bit[idx], val)
+            idx += idx & (-idx)
 
-            # If num is greater than any element in sub
-            if i == len(sub):
-                sub.append(num)
-            
-            # Otherwise, replace the first element in sub greater than or equal to num
-            else:
-                sub[i] = num
-        
-        return len(sub)
+class Solution:  # 360 ms, faster than 69.28%
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        bit = MaxBIT(20001)
+        BASE = 10001
+        for x in nums:
+            subLongest = bit.get(BASE + x - 1)
+            bit.update(BASE + x, subLongest + 1)
+        return bit.get(20001)
