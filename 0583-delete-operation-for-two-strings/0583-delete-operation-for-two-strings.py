3cae64c0104a1ef03faaa8c1c5dcd6eef46f3fd0
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-09-22 20:32:01 +0530

    Time: 91 ms (38.08%), Space: 22.8 MB (42.18%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -25,7 +25,7 @@ class Solution:
             dp[i][0]=i
             for j in range(1,n+1):
                 if word1[i-1]==word2[j-1]:
-                    dp[i][j]=min(dp[i][j],dp[i-1][j-1])
+                    dp[i][j]=dp[i-1][j-1]
                 else:
-                    dp[i][j]=min(dp[i][j],dp[i-1][j]+1,dp[i][j-1]+1)
+                    dp[i][j]=min(dp[i-1][j]+1,dp[i][j-1]+1)
         return dp[-1][-1]
