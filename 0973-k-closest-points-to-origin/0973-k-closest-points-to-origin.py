676f5fd20e6e1b0d803b450bb30c6dde3c00783f
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-09-23 01:41:11 +0530

    Time: 50 ms (82.66%), Space: 23.7 MB (95.61%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,29 +1,34 @@
+import random
 class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        
-        d = lambda i : points[i][0]**2 + points[i][1]**2
-            
-        def quickselect(l,r):
-            pivot_element=random.randint(l,r)
-            points[pivot_element],points[r]=points[r],points[pivot_element]
-            pivot=d(r)
-            pi=l
-            for i in range(l,r):
-                if d(i)<=pivot:
-                    points[pi],points[i]=points[i],points[pi]
-                    pi+=1
-            points[pi],points[r]=points[r],points[pi]
-            return pi
-            
-        n=len(points)
-        l,r=0,n-1
-        k=k-1  ###IMPORTANT STEP
-        while l<r:
-            pivot_index=quickselect(l,r)
-            if pivot_index==k:
-                return points[:k+1]
-            elif pivot_index<k:
-                l=pivot_index+1
+
+    def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
+        def dist_sq(p: list[int]) -> int:
+            return p[0] * p[0] + p[1] * p[1]
+
+        target_idx = k - 1
+        left, right = 0, len(points) - 1
+
+        while left <= right:
+            pivot_dist = dist_sq(points[random.randint(left, right)])
+            lt, i, gt = left, left, right
+
+            while i <= gt:
+                d = dist_sq(points[i])
+                if d < pivot_dist:
+                    points[lt], points[i] = points[i], points[lt]
+                    lt += 1
+                    i += 1
+                elif d > pivot_dist:
+                    points[gt], points[i] = points[i], points[gt]
+                    gt -= 1
+                else:
+                    i += 1
+
+            if lt <= target_idx <= gt:
+                break
+            elif target_idx < lt:
+                right = lt - 1
             else:
-                r=pivot_index-1
-        return points[:l+1]
+                left = gt + 1
+
+        return points[:k]
