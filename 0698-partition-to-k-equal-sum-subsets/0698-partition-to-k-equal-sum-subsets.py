8987417050752a6fce4bce90d715c76706c0a4a7
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-09-23 00:24:27 +0530

    Time: 1496 ms (14.16%), Space: 26.4 MB (19.51%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -4,14 +4,18 @@ class Solution:
         arr.sort(reverse=True)
         target,rem =divmod(sum(arr),k)
         if rem or arr[0]>target:return False
-        
+        count=0
         dp={0:0}
         for mask in range(2**n):
             if mask not in dp:continue
             for i,num in enumerate(arr):
                 if not (mask & 1<<i) and dp[mask]+num<=target:
                     dp[mask|1<<i]=(dp[mask]+num)%target
+                    if dp[mask|1<<i]==0:
+                        count+=1
+        print(count)
  
         return (1<<n)-1 in dp and dp[(1<<n)-1]==0
-
+        ## All elements are used and Sum of all elements/target_sum==0 makes sure
+        ## we have the requied partition
         # %target is used to make the entire subset zero
