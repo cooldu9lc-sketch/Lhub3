99b68dde4f7d5872f2e511c50d5f5b252864a4b0
cooldu9lc-sketch <cooldu9lc@gmail.com>
2026-09-22 20:52:41 +0530

    Time: 230 ms (74.3%), Space: 19.6 MB (69.8%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,6 +1,6 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        m,n=len(s),len(t)
+        """m,n=len(s),len(t)
         dp=[[0]*(n+1) for _ in range(m+1)]
         for i in range(m+1):
             dp[i][0]=1 ## This is initialized to 1 (not i)
@@ -11,4 +11,17 @@ class Solution:
                 else:
                     dp[i][j]=dp[i-1][j]
         return dp[m][n]
+        """
+        m,n=len(s),len(t)
+        dp=[0]*(n+1) 
         
+        for i in range(1,m+1):
+            diag=1
+            for j in range(1,n+1):
+                next_diag=dp[j]
+                if s[i-1]==t[j-1]:
+                    dp[j]+=diag
+                else:
+                    dp[j]=dp[j]
+                diag=next_diag
+        return dp[-1]
